da20cade9640dd1f91d86a2cdf7fb2f97c6e2c9e
Germanchrystan <germanchrystan@gmail.com>
2023-08-27 14:41:48 -0300

    started level transition system

---
+++ Assets/Scripts/Utils/TriggerChecker.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerChecker: MonoBehaviour
+{
+    [Header("Extra Config")]
+    public string validTag;
+
+    [Header("Events")]
+    public UnityEvent onTriggerEnter;
+    public UnityEvent onTriggerStay;
+    public UnityEvent onTriggerExit;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag(validTag))
+        {
+            if (onTriggerEnter != null)
+            {
+                onTriggerEnter.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerStay2d(Collider2D collision)
+    {
+        if(collision.CompareTag(validTag))
+        {
+            if (onTriggerStay != null)
+            {
+                onTriggerStay.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(validTag))
+        {
+            if (onTriggerExit != null)
+            {
+                onTriggerExit.Invoke();
+            }
+        }
+    }
+}
